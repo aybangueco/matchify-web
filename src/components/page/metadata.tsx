4f45c82b6560974c037c71ad744842eda560bf46
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-10-07 22:26:10 +0800

    feat: add reusable metadata prop

---
+++ src/components/page/metadata.tsx
@@ -0,0 +1,13 @@
+type MetadataProps = {
+	title: string;
+	description: string;
+};
+
+export default function Metadata({ title, description }: MetadataProps) {
+	return (
+		<>
+			<title>{title}</title>
+			<meta name="description" content={description} />
+		</>
+	);
+}
