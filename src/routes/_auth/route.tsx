76a6d70d70f588beefd243ff110918a057a5aeed
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-10-07 18:17:37 +0800

    feat: move auth layout inside _auth folder

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth")({
-	component: RouteComponent,
+	component: AuthLayout,
 });
 
-function RouteComponent() {
+function AuthLayout() {
 	return (
 		<section className="h-screen p-3">
 			<Outlet />
