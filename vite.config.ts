dc5fb132b24ef421f6d4cd01a71403e5e5887136
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-10-07 15:05:51 +0800

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import tanstackRouter from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
+import "vitest/config"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
